cd11b02ea67d9ff309cfb2f19986c10e7374f633
Coralie Delplace <coralie.delplace@gmail.com>
2018-06-14 23:06:53 +0200

    OkFiltre

--- app/src/main/java/fr/wcs/winnewshackathon3/LocationActivity.java
+++ app/src/main/java/fr/wcs/winnewshackathon3/LocationActivity.java
@@ -30,89 +30,98 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
 
 
-            textView = (TextView) findViewById(R.id.textView);
-            distance = (TextView) findViewById(R.id.textViewdistance);
-            button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        distance = (TextView) findViewById(R.id.textViewdistance);
+        button = (Button) findViewById(R.id.button);
 
-            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-
-
-            listener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-
-                    double longitude = location.getLongitude();
-                    double latitude = location.getLatitude();
-                    Location loc1 = new Location("");
-                    loc1.setLatitude(longitude);
-                    loc1.setLongitude(latitude);
-
-                    Location loc2 = new Location("");
-                    loc2.setLatitude(latitude);
-                    loc2.setLongitude(longitude);
-
-                    float distanceInMeters = loc1.distanceTo(loc2);
-
-                    distance.append("n1"+ distanceInMeters);
-
-                    textView.append("n " + longitude + " " + latitude);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 
-                }
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
 
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                Location loc1 = new Location("");
+                loc1.setLatitude(longitude);
+                loc1.setLongitude(latitude);
 
-                }
+                Location loc2 = new Location("");
+                loc2.setLatitude(latitude);
+                loc2.setLongitude(longitude);
 
-                @Override
-                public void onProviderEnabled(String s) {
+                float distanceInMeters = loc1.distanceTo(loc2);
 
-                }
+                distance.append("n1" + distanceInMeters);
 
-                @Override
-                public void onProviderDisabled(String s) {
+                textView.append("n " + longitude + " " + latitude);
 
-                    Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(i);
-                }
-            };
 
-            configure_button();
-        }
-
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-        @NonNull int[] grantResults) {
-            switch (requestCode) {
-                case 10:
-                    configure_button();
-                    break;
-                default:
-                    break;
             }
-        }
 
-        void configure_button() {
-            // first check for permissions
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                    PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                            , 10);
-                }
-                return;
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
             }
-            // this code won'textView execute IF permissions are not allowed, because in the line above there is return statement.
-            button.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View view) {
-                    //noinspection MissingPermission
-                    locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+        // this code won'textView execute IF permissions are not allowed, because in the line above there is return statement.
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                if (ActivityCompat.checkSelfPermission(LocationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(LocationActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates("gps", 5000, 0, listener);
 
                 }
             });
@@ -121,4 +130,4 @@ public class LocationActivity extends AppCompatActivity {
 
         }
     }
-}
+
